192a777dff881be46ac92460a2e8431a512f1df4
kalyanaalk <kanza.putri@yahoo.com>
2023-11-16 07:38:46 +0700

    add no 10

--- eisen.sh
+++ eisen.sh
@@ -1,6 +1,8 @@
 echo 'nameserver 192.168.122.1' > /etc/resolv.conf
 apt-get update
 apt-get install nginx git php7.3 php7.3-fpm -y
+apt-get install apache2-utils -y
+
 service php7.3-fpm start
 service nginx start
 echo '
@@ -19,6 +21,9 @@ server {
                 proxy_set_header    X-Real-IP $remote_addr;
                 proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
                 proxy_set_header    Host $http_host;
+
+                auth_basic "Restricted";
+                auth_basic_user_file /etc/nginx/rahasiakita/.htpasswd;
         }
 
         error_log /var/log/nginx/lb_error.log;
@@ -29,4 +34,8 @@ server {
 
 ln -s /etc/nginx/sites-available/lb-php /etc/nginx/sites-enabled
 unlink /etc/nginx/sites-enabled/default
+
+mkdir /etc/nginx/rahasiakita/
+htpasswd -bc /etc/nginx/rahasiakita/.htpasswd netics ajkA03
+
 service nginx restart
