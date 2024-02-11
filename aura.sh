80b197e1fe111e58ec72ca0bf1390c51380d5ae8
kalyanaalk <kanza.putri@yahoo.com>
2024-02-11 19:02:26 +0700

    edit 1

--- aura.sh
+++ aura.sh
@@ -1,5 +1,5 @@
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 192.170.0.0/16
 apt-get update
 apt-get install isc-dhcp-relay -y
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-service isc-dhcp-relay start
+service isc-dhcp-relay
+oke
